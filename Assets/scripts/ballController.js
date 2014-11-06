33f6c60acd83f3d763d2009fb44310ec9fab17c2
Gerhold <agerho000@Macintosh-2.local>
2014-11-05 22:08:06 -0800

    double level blocks, destroy blocks, reset ball

--- Assets/scripts/ballController.js
+++ Assets/scripts/ballController.js
@@ -1,9 +1,37 @@
-﻿#pragma strict
+﻿var speed:float = 4250;
+var ball:GameObject;
+var bricksound:AudioClip;
+var wallsound:AudioClip;
+var block:GameObject;
 
 function Start () {
 
 }
 
 function Update () {
+	if (transform.position.y < -100)
+	{
+		// lives -1
+		// reset ball, destroy it
+		
+		transform.position.y = GameObject.FindGameObjectWithTag("paddle").transform.position.y+5.5;
+		transform.position.x = GameObject.FindGameObjectWithTag("paddle").transform.position.x;
+		ball.rigidbody.Sleep();
+		
+		var pController:paddleController;
+		pcontroller = GameObject.FindGameObjectWithTag("paddle").GetComponent(paddleController);
+		pcontroller.attachedBall = this.gameObject;
+	}
+}
 
+function OnCollisionEnter(col:Collision) {
+
+	if (col.gameObject.tag == "wall") {
+		audio.PlayOneShot(wallsound, 0.5);
+	}
+	
+	if (col.gameObject.tag == "block") {
+		audio.PlayOneShot(bricksound, 0.5);
+		Destroy(col.gameObject);
+	}
 }
---
+++ Assets/scripts/gameController.js
@@ -0,0 +1,27 @@
+﻿var block:GameObject;
+
+function Start () {
+
+	createBlocks();
+
+}
+
+function Update () {
+
+}
+
+function createBlocks() {
+
+	for (var i : int = -5; i < 6; i++)
+	{
+		Debug.Log("Creating block number: " + i);
+		Instantiate(block, Vector3(i*21, 10, 0), Quaternion.identity);
+	}
+	
+	for (var j : int = -5; j < 6; j++)
+	{
+		Debug.Log("Creating block number: " + j);
+		Instantiate(block, Vector3(j*21, 20, 0), Quaternion.identity);
+	}	
+
+}
